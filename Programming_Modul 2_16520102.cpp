f8acf63e130fc96ae2cc83d90d8154e0593a0d7d
Naufal Anargya <naufalanargyar@gmail.com>
2021-03-10 00:08:07 +0700

    modul 2 uro revisi

--- Programming_Modul 2_16520102.cpp
+++ Programming_Modul 2_16520102.cpp
@@ -1,66 +1,109 @@
-#include <stdio.h>
-int main(){
- int pilih,i,hasil=1;
- float A,B, result;
- printf("Angka pertama: ");
- scanf("%f", &A);
- printf("Angka kedua: ");
- scanf("%f", &B);
- printf("Berikut adalah operator-operator yang tersedia: \n");
- printf("1. Penjumlaha\n");
- printf("2. Pengurangan\n");
- printf("3. Perkalian\n");
- printf("4. Pembagian\n");
- printf("5. Perpangkatan\n");
- printf("6. Pengintegralan\n");
- printf("Pilih program yang diinginkan: " );
- scanf("%d", &pilih);
- if (pilih==1){
-  result = A + B;
-  printf("%.2f",result);
-  break;
- }
- else if (pilih==2){
-  result = A - B;
-  printf("%.2f",result);
-  break;
- }
- else if (pilih==3){
-  result = A * B;
-  printf("%.2f",result);
-  break;
- }
- else if (pilih==4){
-  result = A / B;
-  printf("%.2f",result);
-  break;
- }
- else if  (pilih==5){
-   printf("1. A sebagai bilangan yang akan dipangkatkan dan B sebagai pangkat\n");
-   printf("2. B sebagai bilangan yang akan dipangkatkan dan A sebagai pangkat\n");
-   printf("Pilih kondisi: ");
-   scanf("%d",&pilih);
-   if (pilih==1) {
-    for(i=1;i<=B;i++){
-     hasil=hasil*A;
-    }
-    printf("Hasilnya adalah %d", hasil);
-   }
-   else if (pilih==2) {
-    for(i=1;i<=A;i++){
-     hasil=hasil*B;
-    }
-    printf("Hasilnya adalah %d", hasil);
-   }
-  break;
- }
-  else if (pilih==6){
-  break;
- }
- printf("Apakah anda masih ingin menggunakan kalkulator ini (1/0)?");
- scanf("%d",pilih);
- if (pilih==1){
-  /*mau ngulang programnya*/
- }
- else return 0;
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float pangkat(float a, int b){ 
+    int hasil = 1;
+    while (b != 0){
+        hasil *= a;
+        --b;
+    };
+    
+    return hasil;
 }
+
+float f(float x){
+    return (x*x);
+}
+
+int main()
+{
+    int opt;
+    cout << "Pilih Mode kalkulator\n";
+    cout << "1. Basic Math\n";
+    cout << "2. Integration\n";
+    cin  >> opt;
+    
+    if(opt == 1){
+        float num1,num2 ;
+        int opt2;
+        cout << "Pilih Operasi yang Diinginkan\n";
+        cout << "1. Penjumlahan\n";
+        cout << "2. Pengurangan\n";
+        cout << "3. Perkalian\n";
+        cout << "4. Pembagian\n";
+        cout << "5. Perpangkatan\n";
+        cin >> opt2;
+        if(opt2 == 1){
+          cout << "Masukkan angka pertama = ";
+          cin >> num1;
+          cout << "Masukan angka kedua = ";
+          cin >> num2;
+          cout << num1 + num2;
+        }
+        else if (opt2 == 2){
+          cout << "Masukkan angka pertama = ";
+          cin >> num1;
+          cout << "Masukan angka kedua = ";
+          cin >> num2;
+          cout << num1 - num2;
+        }
+        else if (opt2 == 3){
+          cout << "Masukkan angka pertama = ";
+          cin >> num1;
+          cout << "Masukan angka kedua = ";
+          cin >> num2;
+          cout << num1 * num2;
+        }
+        else if (opt2 == 4){
+          cout << "Masukkan angka pertama = ";
+          cin >> num1;
+          cout << "Masukan angka kedua = ";
+          cin >> num2;
+          cout <<  num1 / num2;
+        }
+        else if (opt2 == 5){
+          cout << "Masukkan angka pertama = ";
+          cin >> num1;
+          cout << "Masukan angka kedua = ";
+          cin >> num2;
+          cout <<  pangkat(num1,num2);
+        }
+        else {
+          cout << "Input Tidak Tersedia";
+        } 
+    } else if (opt == 2){
+            int i,N;
+            float tinggi,bawah,atas,x1,x2,y1,y2,total=0;
+            cout << "Masukkan batas atas = "; 
+            cin >> atas;
+            cout << "Masukkan batas bawah = "; 
+            cin >> bawah;
+            cout << "Masukkan banyak Interval = "; 
+            cin >> N;
+            
+            tinggi=(atas-bawah)/N;
+             // misalkan fungsinya x^2
+            x1= bawah;
+            x2=bawah+tinggi;
+            y1 = f(x1);
+            y2 = f(x2);
+           
+            for (int i=0;i<N;i++)
+        {
+            total+=(y1+y2)*tinggi/2;
+            x1+=tinggi;
+            x2+=tinggi;
+            y1=f(x1);
+            y2=f(x2);
+        }
+        cout << total;
+        } else {
+            cout << "input salah";
+        }
+        
+        // return 0;
+        
+    }
+    
